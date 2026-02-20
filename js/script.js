47465c9eb9684431578257fd805fa190d5917279
Felipe Dias <feliperosignoli120210@gmail.com>
2026-02-19 23:33:22 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
         form.addEventListener('submit', function(e) {
             e.preventDefault();
             const escolha = document.querySelector('input[name="objetivo"]:checked').value;
+            
             const rotas = {
                 'ppl': 'treino-ppl.html',
                 'ppl-up': 'treino-ppl-ul.html', 
-                'abcd': 'treino-abcd.html',
-                'up2x': 'treino-up2x.html'
+                'abcd': 'treino-abcd.html'
             };
+
             window.location.href = rotas[escolha];
         });
     }
